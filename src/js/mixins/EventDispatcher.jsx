6e4e293f581b0edcb43311498e9b746903906ae5
= <=>
2015-12-31 09:03:41 +0800

    add dispatcher

--- src/js/mixins/EventDispatcher.jsx
+++ src/js/mixins/EventDispatcher.jsx
@@ -1,1 +1,24 @@
-var EventDispatcher =
+class EventDispatcher {
+    constructor() {
+        this.map = new WeakMap();
+    }
+
+    hasListeners(event) {
+        return this.map.has(event);
+    }
+
+    addEventListener(event, callback) {
+
+    }
+
+    removeEventListener(event, callback) {
+
+    }
+
+    dispatchEvent(event) {
+
+    }
+}
+
+var dispatcher = new EventDispatcher();
+module.exports = dispatcher;
