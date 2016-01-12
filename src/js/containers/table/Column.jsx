a68d04048fb6b30103a38af55f700f344bfcc9c4
= <=>
2016-01-12 17:57:29 +0800

    add TitleRender and ItemRender

--- src/js/containers/table/Column.jsx
+++ src/js/containers/table/Column.jsx
@@ -1,8 +1,38 @@
+import className from '../../util/className.jsx';
+import ComponentBase from '../../mixins/ComponentBase.jsx';
 import TitleRender from './TitleRender.jsx';
 import ItemRender from './ItemRender.jsx';
+import ItemRenderDefault from './ItemRenderDefault.jsx';
 
 module.exports = React.createClass({
+    mixins:[ComponentBase],
+    getInitialState:function() {
+        return {};
+    },
+    getDefaultProps:function() {
+        return {
+            cname:'table-column'
+        };
+    },
     render:function() {
-        return <tr/>;
+        var classes = className(this.props.className, this.getPropClass());
+
+        var titleRender = null;
+        if(this.props.children) {
+            var children = this.props.children instanceof Array ? this.props.children : [this.props.children];
+            var existTitleRender = children.find(function(child) {
+                return child.props.cname == 'table-title-render';
+            });
+            if(existTitleRender) {
+                titleRender = existTitleRender.props.children;
+            }
+        }
+        if(!titleRender) {
+            titleRender = <ItemRenderDefault dataField={this.props.dataField}/>;
+        }
+
+        return <ul {...this.props} className={classes}>
+
+        </ul>;
     }
 });
