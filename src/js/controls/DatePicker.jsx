bc43d129d784c721fa452ccaefc89199e2a39636
lizihan <darkty2009@gmail.com>
2016-01-14 12:05:06 +0800

    add Datepicker

--- src/js/controls/DatePicker.jsx
+++ src/js/controls/DatePicker.jsx
@@ -2,6 +2,8 @@ import className from '../util/className.jsx';
 import omit from '../util/omit.jsx';
 import ComponentBase from '../mixins/ComponentBase.jsx';
 import Input from './Input.jsx';
+import Calendar from './datepicker/Calendar.jsx';
+
 import '../../css/datepicker.scss';
 
 module.exports = React.createClass({
@@ -17,15 +19,27 @@ module.exports = React.createClass({
         };
     },
     render:function() {
+        var defaultClass = this.getDefaultClass();
         var classes = className(this.props.className, this.getPropClass());
         if(this.state.popup) {
             classes += ' active';
+        }
+        if(this.props.range) {
+            classes += ' range';
         }
 
         var props = omit(this.props, 'value', 'className');
 
         return <div className={classes}>
-            <Input value={this.props.value || ""} {...props} />
+            <Input className={defaultClass+"-inputer"} value={this.props.value || ""} {...props} />
+            <div className={defaultClass+'-popup'}>
+                {this.props.range && (<div className={defaultClass+'-row'}>
+
+                </div>)}
+                <div className={defaultClass+'-row'}>
+
+                </div>
+            </div>
         </div>
     }
 });
---
+++ src/js/controls/datepicker/Calendar.jsx
@@ -0,0 +1,7 @@
+module.exports = React.createClass({
+    render:function() {
+        return <div>
+
+        </div>;
+    }
+});
