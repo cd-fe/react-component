58f450ba48e7d33367de6249200db96cca346565
lizihan <darkty2009@gmail.com>
2016-01-14 12:05:53 +0800

    util omit:clone source object

--- src/js/util/omit.jsx
+++ src/js/util/omit.jsx
@@ -1,7 +1,18 @@
 module.exports = function(obj) {
     var keys = Array.prototype.slice.call(arguments, 1);
-    keys.forEach(function(key) {
-        delete obj[key];
-    });
-    return obj;
+
+    var result = {};
+
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key)) {
+            if(keys.indexOf(key) < 0) {
+                result[key] = obj[key];
+            }
+        }
+    }
+
+    //keys.forEach(function(key) {
+    //    delete obj[key];
+    //});
+    return result;
 };
