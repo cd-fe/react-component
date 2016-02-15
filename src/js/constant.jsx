801d024e995e90afc112f24df15571a7b8a8f3fa
SimpleLee <darkty2009@gmail.com>
2016-02-16 01:17:18 +0800

    half complete

--- src/js/constant.jsx
+++ src/js/constant.jsx
@@ -2,5 +2,8 @@ module.exports = {
     classPrefix:'rui-',
     selected:'selected',
     expand:'expand',
-    treePadding:20
+    treePadding:20,
+    events:{
+        dataChange:'dataChange'
+    }
 };
